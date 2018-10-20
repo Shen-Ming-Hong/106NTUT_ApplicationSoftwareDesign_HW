a87fece650832adea062838ea5fbe2943385abe1
lanskyassassination <lanskyassassination@gmail.com>
2018-10-20 17:46:29 +0800

    add HW_CH3

---
+++ 105360043_沈酩閎_HW_CH3/test_p36/src/test/test.java
@@ -0,0 +1,18 @@
+package test;
+
+public class test {
+	public static void main (String[] args)
+	{
+		String str = "Hello";
+		
+		char ch1 = str.charAt(0);
+		char ch2 = str.charAt(1);
+		
+		int len = str.length();
+
+		System.out.println(str + "第一個字元是" + ch1);
+		System.out.println(str + "第二個字元是" + ch2);
+		System.out.println(str + "的長度是" + len);
+
+	}
+}
